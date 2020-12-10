8c4f556b7ab1094f99ee4eb0138546003f271873
Kevin Wang <kevinwang@MacBook-Pro.Home>
2020-12-10 14:25:42 +0800

    add

--- hw2/SocketExample/server.cpp
+++ hw2/SocketExample/server.cpp
@@ -277,8 +277,7 @@ void *doInChildThread(void *ptr) {
 
                         while(1) {      
                             cap >> imgServer;
-                            cout << send_frame.iptr << endl;
-                            nbytes = send(remoteSocket, &send_frame, sizeof(Frame), 0);
+                            nbytes = send(remoteSocket, imgServer.data, imgSize, 0);
                             if((recved = recv(remoteSocket, &recv_msg, sizeof(Msg), MSG_DONTWAIT)) > 0) {
                                 send_frame.flag = 1;
                                 send(remoteSocket, &send_frame, sizeof(Frame), 0);
